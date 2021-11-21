6d4ffd8a206e12cd6a86e9f60242cbc2200b436b
eraloop <romanricakam@gmail.com>
2021-11-21 03:21:36 +0100

    implemented the scroll view and implementation for the chart section has began

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:expenseapp/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'models/transaction.dart';
 import './widgets/transaction_list.dart';
 import 'widgets/new_transaction.dart';
+import './widgets/chart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,17 @@ class MyApp extends StatelessWidget {
       title: 'FExpense App',
       theme: ThemeData(
         primarySwatch: Colors.purple,
-        accentColor: Colors.amber, 
+        accentColor: Colors.amber,
+        fontFamily:'Quicksand',
+        appBarTheme: AppBarTheme(
+         textTheme: ThemeData.light().textTheme.copyWith(
+           headline1: TextStyle(
+             fontFamily: 'OpenSans',
+              fontSize: 20,
+         )
+        
+         )
+      ),
       ),
       home: Homepage(),
     );
@@ -34,19 +46,27 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
 
     final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'new shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'some tshirts',
-      amount: 300.67,
-      date: DateTime.now(),
-    )
+    // Transaction(
+    //   id: 't1',
+    //   title: 'new shoes',
+    //   amount: 69.99,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'some tshirts',
+    //   amount: 300.67,
+    //   date: DateTime.now(),
+    // )
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+        assert(tx != null);
+        return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7),),);
+    }).toList();
+ }
+
 
   void _addNewTransaction(String title, double amount) {
     final newTx = Transaction(
@@ -76,7 +96,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return  Scaffold(
         appBar: AppBar(
-          title: const Text("Expense App"),
+          title: const Text("Expense App", style: TextStyle(fontFamily: 'OPenSans'),),
           actions: [
             IconButton(icon: Icon(Icons.add),
             onPressed:()=> _startAddNewTransaction(context),)
@@ -90,7 +110,7 @@ class _HomepageState extends State<Homepage> {
             Container(
               width: double.infinity,
               child:
-                  Card(child: Text("Chart"), elevation: 5, color: Colors.blue),
+                  Chart(_recentTransactions)
 
             ),
            TransactionList(_userTransactions)],
